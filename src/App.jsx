b8bb0840c09700ab3535a79db40a1c8fc034777e
DanielK <daniel.rt5d@gmail.com>
2026-03-26 10:25:08 -0500

    Actualizar componentes Form y ColorList para manejar la adición de colores dinámicamente y ajustar la lógica de visualización en OneColor

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,19 @@ import { toast, ToastContainer } from "react-toastify";
 
 const App = () => {
   
-  const [colors, setColors] = useState(new Values('#a1161f').all(10));
+  const [colors, setColors] = useState(new Values('#a1161f').all(5));
+  const addColor = (color) =>{
+    try {
+      const newColor = new Values(color).all(5);
+      setColors(newColor)
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
   return (
     <main>
       <ToastContainer position="top-right"/>
-      <Form />
+      <Form  addColor={addColor}/>
       <ColorList colors={colors}/>
     </main>
   );
--- src/OneColor.jsx
+++ src/OneColor.jsx
@@ -5,7 +5,7 @@ const OneColor = ({index, color}) => {
   const {hex, weight} = color;
 
   return (
-    <article className={index>10?'color color-light':'color'} style={{background:`#${hex}`}}>
+    <article className={index>20?'color color-light':'color'} style={{background:`#${hex}`}}>
       <p className='percent-value'>{weight}%</p>
       <p className='color-value'>#{hex}</p>
     </article>

79ddca19c4dd690d34d3ad7f700eab448fb451ef
DanielK <daniel.rt5d@gmail.com>
2026-03-26 11:26:30 -0500

    Agregar funcionalidad para copiar el color al portapapeles en el componente OneColor

--- src/OneColor.jsx
+++ src/OneColor.jsx
@@ -4,9 +4,21 @@ import { getTextColor } from './utils';
 const OneColor = ({color}) => {
   const {hex, weight} = color;
   const textColor = getTextColor(hex);
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+        toast.success(`Color #${hex} copiado al portapapeles!`);
+      } catch (err) {
+        toast.error('Error al copiar el color: ' + err.message);
+      }
+    }else {
+      toast.error('El portapapeles no es compatible con este navegador.');
+    }
+  }
 
   return (
-    <article className='color' style={{background:`#${hex}`}}>
+    <article className='color' style={{background:`#${hex}`}} onClick={saveToClipboard}>
       <p className='percent-value' style={{color: textColor}}>{weight}%</p>
       <p className='color-value' style={{color: textColor}}>#{hex}</p>
     </article>
